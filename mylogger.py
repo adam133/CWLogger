e0f2f6065861b95678679974979f9e54489963b6
Adam Best <besta2010@gmail.com>
2019-09-03 19:58:18 -0500

    Formatted and minor changes to the stream naming to be more general

--- mylogger.py
+++ mylogger.py
@@ -1,76 +1,78 @@
 import boto3
 import time
 
-defaultLogGroup = 'databricks-etl/notebooks/customlogs'
+defaultLogGroup = 'MyCustomLogs/Logs'
 defaultLogStream = 'Notebook-logging-'
+region = 'us-east-1'
 
-class cwlog:
-    '''Represents a logger object that keeps track of cloudwatch logging variables'''
-    
+
+class CWLog:
+    """Represents a logger object that keeps track of cloudwatch logging variables"""
+
     def __init__(self, logStreamName=None, logGroup=None):
-        '''Initializes the logger object
-        
+        """Initializes the logger object
+
         The logStreamName is the name given to this instance of the logger
-        The logGroup is the group of logs to put this instance in
-        '''
-        
-        self.cwl =  boto3.client('logs',region_name='us-east-1')
-        
+        A timestamp will be appended in the format "logStreamName_9999999999"
+        The logGroup is the cloudwatch group of logs to put this instance in
+        """
+
+        self.cwl = boto3.client('logs', region_name=region)
+
         if logGroup is None:
             logGroup = defaultLogGroup
-        
-        #Check if the logGroup exists
-        logGroups = self.cwl.describe_log_groups(logGroupNamePrefix=logGroup)
-        
+
+            # Check if the logGroup exists
+            logGroups = self.cwl.describe_log_groups(logGroupNamePrefix=logGroup)
+
         if not logGroups['logGroups']:
-            #No logGroups match, need to create it
+            # No logGroups match, need to create it
             self.cwl.create_log_group(logGroupName=logGroup)
-        
-        self.logGroup = logGroup
-        #Set a stream name if none given.
+
+            self.logGroup = logGroup
+            # Set a stream name if none given.
         if logStreamName is None:
-            logStreamName = defaultLogStream + str(int(time.time()*1000))
-        
-        self.logStreamName = logStreamName
-        #Create the log stream for all logs in this log instance
-        self.cwl.create_log_stream(logGroupName=logGroup, logStreamName=logStreamName)
-        
-        #Start of log
+            logStreamName = defaultLogStream + '_' + str(int(time.time() * 1000))
+
+            self.logStreamName = logStreamName + '_' + str(int(time.time() * 1000))
+            # Create the log stream for all logs in this log instance
+            self.cwl.create_log_stream(logGroupName=logGroup, logStreamName=logStreamName)
+
+        # Start of log
         rsp = self.cwl.put_log_events(
-               logGroupName=self.logGroup,
-               logStreamName=self.logStreamName,
-               logEvents=[{
-                   'timestamp': int(time.time()*1000),
-                   'message': 'Log started for ' + logStreamName
-                    },
-               ])
-        
-        #Track the next Log token
+            logGroupName=self.logGroup,
+            logStreamName=self.logStreamName,
+            logEvents=[{
+                'timestamp': int(time.time() * 1000),
+                'message': 'Log started for ' + logStreamName
+            },
+            ])
+
+        # Track the next Log token
         self.nextSequenceToken = rsp['nextSequenceToken']
-        
-        #Record the response to the object if it's needed
+
+        # Record the response to the object if it's needed
         self.response = rsp
 
     def log(self, dataToLog):
-        '''Add something to the existing log'''
-        
+        """Add something to the existing log"""
+
         rsp = self.cwl.put_log_events(
-               logGroupName=self.logGroup,
-               logStreamName=self.logStreamName,
-               logEvents=[{
-                   'timestamp': int(time.time()*1000),
-                   'message': dataToLog
-                    },
-               ],
-               sequenceToken = self.nextSequenceToken)
-        
-        #update the Sequence token
+            logGroupName=self.logGroup,
+            logStreamName=self.logStreamName,
+            logEvents=[{
+                'timestamp': int(time.time() * 1000),
+                'message': dataToLog
+            },
+            ],
+            sequenceToken=self.nextSequenceToken)
+
+        # update the Sequence token
         self.nextSequenceToken = rsp['nextSequenceToken']
-        
+
         rsp['logged-value'] = dataToLog
 
         print(dataToLog)
-        
-        #Record the response in case the caller needs it
-        self.response = rsp
 
+        # Record the response in case the caller needs it
+        self.response = rsp
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
+
 setup(name='mylogger',
       version='1.0',
-      py_modules=['mylogger'],
-     )
+      py_modules=['mylogger'], install_requires=['boto3']
+      )
