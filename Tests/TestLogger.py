636cab1cf434f70771269415a81937b7447475e1
adambest <besta2010@gmail.com>
2019-09-06 17:39:37 -0500

    Added a few more in-depth test cases.

--- Tests/TestLogger.py
+++ Tests/TestLogger.py
@@ -3,11 +3,51 @@ import mylogger
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_basicLog(self):
         log = mylogger.CWLog()
-        log.log("Successfully Executed...")
-        self.assertEqual(True, True)
+        text = "Success"
+        log.log(text)
 
+        rspText = log.response['logged-value']
+
+        self.assertEqual(text, rspText)
+
+    def test_specify_group(self):
+        group = 'MyTestLogGroup'
+        log = mylogger.CWLog(logGroup=group)
+        text = "Success"
+        log.log(text)
+        rspGroup = log.logGroup
+
+        self.assertEqual(group, rspGroup)
+
+    def test_specify_stream(self):
+        stream = 'MyTestStream'
+
+        log = mylogger.CWLog(logStreamName=stream)
+        text = "Success"
+        log.log(text)
+        rspStream = log.logStreamName
+
+        self.assertEqual(rspStream.startswith(stream), True)
+
+    def test_specify_group_and_stream(self):
+        stream = 'MyTestStream'
+        group = 'MyTestLogGroup'
+
+        log = mylogger.CWLog(logStreamName=stream, logGroup=group)
+
+        text = "Success"
+
+        log.log(text)
+
+        rspStream = log.logStreamName
+        rspGroup = log.logGroup
+        rspText = log.response['logged-value']
+
+        self.assertEqual(rspStream.startswith(stream), True)
+        self.assertEqual(group, rspGroup)
+        self.assertEqual(text, rspText)
 
 if __name__ == '__main__':
     unittest.main()
